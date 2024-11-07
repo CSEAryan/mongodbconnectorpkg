60da176e86d26191ab3333b696a4c359828d8604
CSEAryan <dipuaryan1111@gmail.com>
2024-11-07 22:35:07 +0545

    updated code and version

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
+PKG_NAME= "mongodatabaseautomation"
 AUTHOR_USER_NAME = "CSEAryan"
 AUTHOR_EMAIL = "dipuaryan1111@gmail.com"
 
