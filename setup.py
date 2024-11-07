048963031b95803352eaabacc3342aad1e699f32
CSEAryan <dipuaryan1111@gmail.com>
2024-11-07 23:19:10 +0545

    updated with setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ def get_requirement(file_path:str)->List[str]:
     return requirements
 
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
+#with open('README.md', 'r', encoding='utf-8') as f:
+    #long_description = f.read()     
    
 
 __version__ = "0.0.5"
@@ -30,8 +30,8 @@ setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+   # long_description=long_description,
+   # long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
